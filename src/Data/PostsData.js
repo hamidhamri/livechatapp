4dcaeba0fce3130c0554e4221a161336d0a7cfe4
hamidhamri <hamid-raw@hotmail.fr>
2022-08-22 00:52:51 +0100

    one of the last one inchallahh

---
+++ src/Data/PostsData.js
@@ -0,0 +1,26 @@
+export const PostsData = [
+  {
+    img: "/images/postpic1.jpg",
+    profileImg: "/images/img1.png",
+    name: "Tzuyu",
+    description: "Happy New Year all friends! #2023",
+    likes: 2300,
+    liked: true,
+  },
+  {
+    img: "/images/postpic2.jpg",
+    profileImg: "/images/img3.png",
+    name: "Maryam",
+    description: "Party time :)",
+    likes: 2300,
+    liked: false,
+  },
+  {
+    img: "/images/postpic3.JPG",
+    profileImg: "/images/img2.png",
+    name: "Salena Gomez",
+    description: "At Archery Festival",
+    likes: 800,
+    liked: false,
+  },
+];
