40636de7d9a518909b7f3b3e7e64b7c2c21c4b1e
hamidhamri <hamid-raw@hotmail.fr>
2022-08-30 14:21:09 +0100

    after sanko gali bug

--- src/components/smallComponents/ProfileCard.jsx
+++ src/components/smallComponents/ProfileCard.jsx
@@ -133,7 +133,7 @@ const ProfileCard = ({ location }) => {
         });
         const now = Date.now() + userInfo.data._id;
         await axios.put(
-          `https://hamidhamrichatapp.blob.core.windows.net/chatapp/${now}.jpg?sp=acw&st=2022-08-25T21:06:19Z&se=2022-08-26T05:06:19Z&sv=2021-06-08&sr=c&sig=4ym7F%2BnQfV1%2FYsnNVBXal3QiAOPb7DbsMffNvIlPvb4%3D`,
+          `https://hamidhamrichatapp.blob.core.windows.net/chatapp/${now}.jpg?sv=2021-06-08&ss=bf&srt=o&sp=rwdactfx&se=2023-08-30T21:17:22Z&st=2022-08-30T13:17:22Z&spr=https&sig=zMcLJ%2FVsNyETLwe29lkHy9WcfrgIpHpUxJS59n1JLV8%3D`,
           blob,
           {
             headers: {
